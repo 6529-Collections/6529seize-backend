d8f69425f569d9b30605c32f656a3b05728e530b
prxt <prxt@6529.io>
2023-01-27 10:07:57 +0200

    Trying without installer

--- src/s3.ts
+++ src/s3.ts
@@ -13,11 +13,6 @@ import ffmpeg from 'fluent-ffmpeg';
 
 const imagescript = require('imagescript');
 
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg');
-console.log('ffmpeg', ffmpegPath.path, ffmpegPath.version);
-
-ffmpeg.setFfmpegPath(ffmpegPath.path);
-
 const THUMBNAIL_HEIGHT = 450;
 const SCALED_HEIGHT = 1000;
 const config = require('./config');
