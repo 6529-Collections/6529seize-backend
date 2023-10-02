1c406ed2e36d3496b4132fde93f9760b3bc60d22
prxt <prxt@6529.io>
2023-10-02 15:18:25 +0300

    Start script
    Signed-off-by: prxt <prxt@6529.io>

--- src/backend.ts
+++ src/backend.ts
@@ -104,13 +104,13 @@ async function start() {
 
   // Uncomment to call on start
 
-  // await nftHistory.handler();
-  // await delegations.handler();
-  // await transactions.handler();
-  // await nfts.handler();
-  // await memeLab.handler();
-  // await ownerMetrics.handler();
-  // await tdh.handler();
+  await nftHistory.handler();
+  await delegations.handler();
+  await transactions.handler();
+  await nfts.handler();
+  await memeLab.handler();
+  await ownerMetrics.handler();
+  await tdh.handler();
   // await tdhHistory.handler();
   // await memeStats();
   // await gradientStats();
