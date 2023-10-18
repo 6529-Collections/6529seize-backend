88e6ba6b8f59151bb77585a7cf1f673354844693
prxt <prxt@6529.io>
2023-10-18 12:59:14 +0300

    WIP - move owners to nfts loop
    Signed-off-by: prxt <prxt@6529.io>

--- src/ownersLoop/index.ts
+++ src/ownersLoop/index.ts
@@ -1,18 +1,16 @@
-import { findOwners } from '../owners';
 import { findOwnerTags } from '../owners_tags';
 import { loadEnv, unload } from '../secrets';
 import { NFT } from '../entities/INFT';
 import { ConsolidatedOwnerTags, Owner, OwnerTags } from '../entities/IOwner';
 
 export const handler = async () => {
-  console.log(new Date(), '[RUNNING OWNERS LOOP]');
+  console.log(new Date(), '[RUNNING OWNER TAGS LOOP]');
   await loadEnv([NFT, Owner, OwnerTags, ConsolidatedOwnerTags]);
   await ownersLoop();
   await unload();
-  console.log(new Date(), '[OWNERS LOOP COMPLETE]');
+  console.log(new Date(), '[OWNER TAGS LOOP COMPLETE]');
 };
 
 async function ownersLoop() {
-  await findOwners();
   await findOwnerTags();
 }
