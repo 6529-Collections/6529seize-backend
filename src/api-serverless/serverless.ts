c02bf35b958fcd2cf232cad7fa758af3b87a9e62
tarmo <tarmokalling@gmail.com>
2023-11-06 13:42:02 +0200

    wip

--- src/api-serverless/serverless.ts
+++ src/api-serverless/serverless.ts
@@ -1,6 +1,7 @@
 import type { AWS } from '@serverless/typescript';
 
 import { handler } from './src/handler';
+import * as process from 'process';
 
 const serverlessConfiguration: AWS = {
   service: 'api-serverless',
@@ -15,7 +16,17 @@ const serverlessConfiguration: AWS = {
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000'
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      DB_HOST: process.env.DB_HOST,
+      DB_HOST_READ: process.env.DB_HOST_READ,
+      DB_NAME: process.env.DB_NAME,
+      DB_PASS: process.env.DB_PASS,
+      DB_PASS_READ: process.env.DB_PASS_READ,
+      DB_PORT: process.env.DB_PORT,
+      DB_USER: process.env.DB_USER,
+      DB_USER_READ: process.env.DB_USER_READ,
+      SENTRY_DSN: process.env.SENTRY_DSN,
+      SEIZE_API_VERSION: process.env.SEIZE_API_VERSION
     }
   },
   // import the function via paths
