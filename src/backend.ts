204f21a689d143eb3f288ba583c2e7bfcc213ab5
prxt <prxt@6529.io>
2023-11-15 15:10:52 +0200

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- src/backend.ts
+++ src/backend.ts
@@ -135,10 +135,9 @@ async function start() {
   // await transactions.handlerValues();
   // await rememes.handler();
   // await overvotesRevocation.handler();
-  // await nextgenLoop.handler();
   await transactionsReplay.handler();
 
-  // RUNNING_START_SCRIPT = false;
+  RUNNING_START_SCRIPT = false;
   logger.info(`[START SCRIPT COMPLETE] [SERVICE STARTED...]`);
 }
 
