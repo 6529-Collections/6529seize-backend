8d28262fbb84df3a56648632e326790fdd7d0ac5
tarmo <tarmokalling@gmail.com>
2023-11-16 10:40:11 +0200

    Profiles archive and generic native transactions support
    Signed-off-by: tarmo <tarmokalling@gmail.com>

--- src/entities/IProfile.ts
+++ src/entities/IProfile.ts
@@ -1,9 +1,51 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
-import { PROFILES_TABLE } from '../constants';
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
+import { PROFILES_ARCHIVE_TABLE, PROFILES_TABLE } from '../constants';
 
 @Entity(PROFILES_TABLE)
 export class Profile {
   @PrimaryColumn({ type: 'varchar', length: 100 })
+  normalised_handle!: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  handle!: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  primary_wallet!: string;
+
+  @Column({ type: 'datetime' })
+  created_at!: Date;
+
+  @Column({ type: 'varchar', length: 50 })
+  created_by_wallet!: string;
+
+  @Column({ type: 'datetime', nullable: true, default: null })
+  updated_at?: Date;
+
+  @Column({ type: 'varchar', length: 50, nullable: true, default: null })
+  updated_by_wallet?: string;
+
+  @Column({ type: 'text', nullable: true, default: null })
+  pfp_url?: string;
+
+  @Column({ type: 'text', nullable: true, default: null })
+  banner_1?: string;
+
+  @Column({ type: 'text', nullable: true, default: null })
+  banner_2?: string;
+
+  @Column({ type: 'text', nullable: true, default: null })
+  website?: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true, default: null })
+  classification?: ProfileClassification | null;
+}
+
+@Entity(PROFILES_ARCHIVE_TABLE)
+export class ProfileArchived {
+  @PrimaryGeneratedColumn('increment', { type: 'bigint' })
+  id!: number;
+
+  @Column({ type: 'varchar', length: 100 })
   normalised_handle!: string;
 
   @Column({ type: 'varchar', length: 100 })
--- src/overvotesRevocationLoop/index.ts
+++ src/overvotesRevocationLoop/index.ts
@@ -2,14 +2,14 @@ import { loadEnv, unload } from '../secrets';
 import * as votes from '../votes';
 import { VoteMatterCategory } from '../entities/IVoteMatter';
 import { VoteEvent } from '../entities/IVoteEvent';
-import { Profile } from '../entities/IProfile';
+import { Profile, ProfileArchived } from '../entities/IProfile';
 import { Logger } from '../logging';
 
 const logger = Logger.get('OVERVOTES_REVOCATION_LOOP');
 
 export const handler = async () => {
   logger.info(`[RUNNING]`);
-  await loadEnv([VoteMatterCategory, VoteEvent, Profile]);
+  await loadEnv([VoteMatterCategory, VoteEvent, Profile, ProfileArchived]);
   await votes.revokeOverVotes();
   await unload();
   logger.info(`[COMPLETE]`);
