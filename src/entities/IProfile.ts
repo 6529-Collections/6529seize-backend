a8049684d60ee705ddcb09d192a8e61d5d65e467
tarmo <tarmokalling@gmail.com>
2023-11-16 18:42:17 +0200

    Refactored IProfile.ts
    Signed-off-by: tarmo <tarmokalling@gmail.com>

--- src/entities/IProfile.ts
+++ src/entities/IProfile.ts
@@ -1,11 +1,7 @@
 import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
 import { PROFILES_ARCHIVE_TABLE, PROFILES_TABLE } from '../constants';
 
-@Entity(PROFILES_TABLE)
-export class Profile {
-  @PrimaryColumn({ type: 'varchar', length: 100 })
-  normalised_handle!: string;
-
+class ProfileBase {
   @Column({ type: 'varchar', length: 100 })
   handle!: string;
 
@@ -40,46 +36,19 @@ export class Profile {
   classification?: ProfileClassification | null;
 }
 
+@Entity(PROFILES_TABLE)
+export class Profile extends ProfileBase {
+  @PrimaryColumn({ type: 'varchar', length: 100 })
+  normalised_handle!: string;
+}
+
 @Entity(PROFILES_ARCHIVE_TABLE)
-export class ProfileArchived {
+export class ProfileArchived extends ProfileBase {
   @PrimaryGeneratedColumn('increment', { type: 'bigint' })
   id!: number;
 
   @Column({ type: 'varchar', length: 100 })
   normalised_handle!: string;
-
-  @Column({ type: 'varchar', length: 100 })
-  handle!: string;
-
-  @Column({ type: 'varchar', length: 50 })
-  primary_wallet!: string;
-
-  @Column({ type: 'datetime' })
-  created_at!: Date;
-
-  @Column({ type: 'varchar', length: 50 })
-  created_by_wallet!: string;
-
-  @Column({ type: 'datetime', nullable: true, default: null })
-  updated_at?: Date;
-
-  @Column({ type: 'varchar', length: 50, nullable: true, default: null })
-  updated_by_wallet?: string;
-
-  @Column({ type: 'text', nullable: true, default: null })
-  pfp_url?: string;
-
-  @Column({ type: 'text', nullable: true, default: null })
-  banner_1?: string;
-
-  @Column({ type: 'text', nullable: true, default: null })
-  banner_2?: string;
-
-  @Column({ type: 'text', nullable: true, default: null })
-  website?: string;
-
-  @Column({ type: 'varchar', length: 255, nullable: true, default: null })
-  classification?: ProfileClassification | null;
 }
 
 export enum ProfileClassification {
