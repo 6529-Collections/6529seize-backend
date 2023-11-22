0f0ccc58fcd4d2ab70574dbc402c22ceacafefc9
tarmo <tarmokalling@gmail.com>
2023-11-22 09:44:53 +0200

    Refactored rater into a class and covered it with unit tests
    Signed-off-by: tarmo <tarmokalling@gmail.com>

---
+++ src/tests/test.helper.ts
@@ -0,0 +1,27 @@
+import { LazyDbAccessCompatibleService } from '../sql-executor';
+import { Mock, mock } from 'ts-jest-mocker';
+import { when } from 'jest-when';
+
+export async function expectExceptionWithMessage(
+  scenario: () => unknown,
+  message: string
+) {
+  try {
+    await scenario();
+    throw new Error(
+      `Expected an exception with message ${message}, but it never happened`
+    );
+  } catch (e: any) {
+    expect(e?.message).toBe(message);
+  }
+}
+
+export function mockDbService<T extends LazyDbAccessCompatibleService>(
+  clazz?: new (...args: any[]) => T
+): Mock<T> {
+  const mocked = mock(clazz);
+  when(mocked.executeNativeQueriesInTransaction).mockImplementation(
+    async (lambda) => await lambda({ connection: {} })
+  );
+  return mocked;
+}
