79d084ea000cec22cf8a22b54a4e1dc909e877c4
prxt <prxt@6529.io>
2023-11-27 08:48:32 +0200

    Startscript for memelab replay
    Signed-off-by: prxt <prxt@6529.io>

--- src/backend.ts
+++ src/backend.ts
@@ -134,10 +134,10 @@ async function start() {
   // await royaltiesLoop.handler();
   // await transactions.handlerValues();
   // await rememes.handler();
-  // await transactionsReplay.handler();
+  await transactionsReplay.handler();
   // await overRatesRevocation.handler();
 
-  RUNNING_START_SCRIPT = false;
+  // RUNNING_START_SCRIPT = false;
   logger.info(`[START SCRIPT COMPLETE] [SERVICE STARTED...]`);
 }
 
