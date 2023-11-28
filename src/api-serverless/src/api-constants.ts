14d93b32eaa397420fa880f58dbe44e97d9cb559
tarmo <tarmokalling@gmail.com>
2023-11-28 18:22:40 +0200

    CIC relative ratings route
    Signed-off-by: tarmo <tarmokalling@gmail.com>

--- src/api-serverless/src/api-constants.ts
+++ src/api-serverless/src/api-constants.ts
@@ -8,6 +8,7 @@ export const ACCESS_CONTROL_ALLOW_ORIGIN_HEADER =
   'Access-Control-Allow-Headers';
 export const CONTENT_TYPE_HEADER = 'Content-Type';
 export const JSON_HEADER_VALUE = 'application/json';
+export const DEFAULT_PAGE_SIZE = 50;
 export const NFTS_PAGE_SIZE = 101;
 export const DISTRIBUTION_PAGE_SIZE = 250;
 export const SORT_DIRECTIONS = ['ASC', 'DESC'];
