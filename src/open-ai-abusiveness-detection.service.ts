3859a608010f88a1ba8997e878e096cfc56bf2f8
Simo <info@analyticsflow.ee>
2024-01-03 12:06:39 +0200

    improved rep category search chatgpt prompt
    Signed-off-by: Simo <info@analyticsflow.ee>

--- src/open-ai-abusiveness-detection.service.ts
+++ src/open-ai-abusiveness-detection.service.ts
@@ -12,30 +12,43 @@ export class OpenAiAbusivenessDetectionService {
 
   public async checkText(text: string): Promise<AbusivenessDetectionResult> {
     const message = `
+Background
+
 We operate a social media site in the cryptocurrency space, more specifically in the nft space. This website allows us to give "rep" (aka reputation) points to community members for different activities or characteristics.
 some examples below, that show the rep points being assigned and the classification for each rep.
 +1,000 for "Solidity Programming" +500 for "nude photography" +4,200 for "Pizza Cooking" +6,969 for "Kindness"
 4,200 for "phishing" -100 for "Unreliable" +5,000 for "teaching" +7,540 for "History of Carthage" +4,000 for "cold showers" +4,444 for "bitcoin mining"
-You are going to help us create the allowlist of allowed rep classification.
-the general model is that all rep classifications are allowed except the following categories
-Discriminatory or hate speech on any typical grounds
-Personal insults
-Generally words that would make a normal user in the cryptotwitter community feel bad or uncomfortable.
-Given the nature of the community (cryptotwitter and nft twitter) we are more permissive than most social media sites on the following two factors:
-Typical cryptotwitter terms. Aka "shitposting" is fine as a term
-Nudity as we support nude art photographers
-Note further that people may submit classifications in different languages. Apply the same rules in that case, taking into account idiomatic use The format of the task is as follows:
-we will submitted a classification
-you will return: a) the value: "Allowed" or "Disallowed" b) the rationale of why it is disallowed in the case you disallowed
-you must return NO other text or explanations as you will be integrated into an automated workflow and we will not be able to understand the text
+Task
+
+You are going to help us create the allowlist of allowed rep classifications
+The general model is that all rep classifications are allowed except the following five categories
+1. Discriminatory or hate speech on any typical grounds
+2. Personal insults
+3. Generally words that would make a normal user in the cryptotwitter community feel bad or uncomfortable. Given the nature of the community (cryptotwitter and nft twitter) we are more permissive than most social media sites on the following two factors:
+  a. Typical cryptotwitter terms. Aka "shitposting" is fine as a term
+  b. Nudity as we support nude art photographers
+4. Personal Doxxing Information:  This is information that could dox a person’s identity such:
+  a. Proper names (e.g. “John Hammersmith”)
+  b. Indirect workarounds to the above (e.g. “John Hammersmith’s father”)
+  c. You can assume that famous people’s names such are politicians, celebrities and athletes are not a concern and can be allowed
+Organizational Doxxing Information:  This is information that could dox a person’s through their organizational affiliations such as: “CEO of Acme Enterprises” or “VP of Finance, Salesforce.com” or “the person who the VP of Finance at Acme Inc replaced last year”
+Note further that people may submit classifications in different languages. Apply the same rules in that case, taking into account idiomatic use 
+
+Format
+
+The format of the task is as follows:
+We will submit a classification.
+You will return: 
+The value: "Allowed" or "Disallowed"
+The rationale of why it is disallowed, in the case you disallowed
+You must return NO other text or explanations as you will be integrated into an automated workflow and we will not be able to understand the text.
 I will share two examples of input and output.
 Example 1:
-input: "beautiful baby"
+input: "Software Developer "
 Allowed
 Example 2:
 input: "fuckface"
-Disallowed Insulting term
-
+Disallowed - Insulting Term
 I will now put the classification request after the word "input" and make further requestions in that format.
 input: ${text}
     `.trim();
