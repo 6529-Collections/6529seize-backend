e965a760dc06a266954dc15f94b522b65b05ef28
prxt <prxt@6529.io>
2024-01-08 13:52:42 +0200

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- src/nextgenMediaInterceptor/index.ts
+++ src/nextgenMediaInterceptor/index.ts
@@ -1,7 +1,13 @@
 import { notifyMissingNextgenMedia } from '../notifier';
 
 const CF_BASE_PATH = 'https://media-proxy.nextgen-generator.seize.io';
-const PLACEHOLDER_PATH = `${CF_BASE_PATH}/placeholders/pending.png`;
+const DEFAULT_IMAGE_PATH = `${CF_BASE_PATH}/placeholders/pending.png`;
+
+const DEFAULT_RESPONSE_BODY = {
+  name: 'Pending...',
+  description: 'Pending...',
+  image: DEFAULT_IMAGE_PATH
+};
 
 interface Details {
   network: string;
@@ -10,47 +16,31 @@ interface Details {
 }
 
 export const handler = async (event: any) => {
-  const { request, response } = event.Records[0].cf;
+  let { request, response } = event.Records[0].cf;
   try {
     if (response.status === '403') {
       const uri = request.uri;
+      await notifyMissingNextgenMedia(uri);
+
       const details = getDetails(uri);
       const image = getImagePath(details);
-      await notifyMissingNextgenMedia(uri);
-      const customResponse: any = {
-        name: 'Pending...',
-        description: 'Pending...',
-        path: uri,
-        network: details.network,
-        image: image
-      };
+      const customResponse: any = DEFAULT_RESPONSE_BODY;
+      customResponse.path = uri;
+      customResponse.network = details.network;
+      customResponse.image = image;
       if (details.collection) {
         customResponse.collection = details.collection;
       }
       if (details.tokenId) {
         customResponse.tokenId = details.tokenId;
       }
-      response.body = JSON.stringify(customResponse);
-      response.status = '200';
-      response.statusDescription = 'OK';
-      response.headers['content-type'] = [
-        { key: 'Content-Type', value: 'application/json' }
-      ];
+      response = buildJsonResponse(response, customResponse);
     }
   } catch (e) {
     console.error(e);
-    const customResponse: any = {
-      name: 'Pending...',
-      description: 'Pending...',
-      path: request.uri,
-      image: PLACEHOLDER_PATH
-    };
-    response.body = JSON.stringify(customResponse);
-    response.status = '200';
-    response.statusDescription = 'OK';
-    response.headers['content-type'] = [
-      { key: 'Content-Type', value: 'application/json' }
-    ];
+    const customBody: any = DEFAULT_RESPONSE_BODY;
+    customBody.path = request.uri;
+    response = buildJsonResponse(response, customBody);
   }
   return response;
 };
@@ -61,7 +51,7 @@ function getDetails(uri: string): Details {
   }
   const uriSegments = uri.split('/');
   const network = uriSegments[0];
-  const tokenIdStr = uri.split('/').pop();
+  const tokenIdStr = uriSegments.pop();
 
   const tokenId = Number(tokenIdStr);
   if (!isNaN(tokenId)) {
@@ -81,5 +71,15 @@ function getImagePath(details: Details) {
   if (details.collection && details.collection > 0) {
     return `${CF_BASE_PATH}/placeholders/${details.network}/${details.collection}.png`;
   }
-  return PLACEHOLDER_PATH;
+  return DEFAULT_IMAGE_PATH;
+}
+
+function buildJsonResponse(response: any, body: any) {
+  response.body = JSON.stringify(body);
+  response.status = '200';
+  response.statusDescription = 'OK';
+  response.headers['content-type'] = [
+    { key: 'Content-Type', value: 'application/json' }
+  ];
+  return response;
 }
