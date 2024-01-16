431f84136f30fbf77c5ea9bbaca4a8679c52bb8d
tarmo <tarmokalling@gmail.com>
2024-01-16 11:39:22 +0200

    Hooked Sentry to all lambdas
    Signed-off-by: tarmo <tarmokalling@gmail.com>

--- src/s3Loop/index.ts
+++ src/s3Loop/index.ts
@@ -2,16 +2,17 @@ import { fetchAllMemeLabNFTs, fetchAllNFTs } from '../db';
 import { persistS3 } from '../s3';
 import { loadEnv, unload } from '../secrets';
 import { Logger } from '../logging';
+import * as sentryContext from "../sentry.context";
 
 const logger = Logger.get('S3_LOOP');
 
-export const handler = async (event?: any, context?: any) => {
+export const handler = sentryContext.wrapLambdaHandler(async (event?: any, context?: any) => {
   logger.info('[RUNNING]');
   await loadEnv();
   await s3Loop();
   await unload();
   logger.info('[COMPLETE]');
-};
+});
 
 export async function s3Loop() {
   if (process.env.NODE_ENV == 'production') {
