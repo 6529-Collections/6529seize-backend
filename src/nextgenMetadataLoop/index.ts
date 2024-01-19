f104f78939c9c8c9b28e968d446baff53b1555f7
prxt <prxt@6529.io>
2024-01-19 15:19:37 +0200

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- src/nextgenMetadataLoop/index.ts
+++ src/nextgenMetadataLoop/index.ts
@@ -1,7 +1,11 @@
 import { Logger } from '../logging';
 import { Time } from '../time';
 import { loadEnv } from '../secrets';
-import { NextGenToken, NextGenTokenTrait } from '../entities/INextGen';
+import {
+  NextGenCollection,
+  NextGenToken,
+  NextGenTokenTrait
+} from '../entities/INextGen';
 import { refreshNextgenMetadata } from '../nextgen/nextgen_metadata_refresh';
 
 const logger = Logger.get('NEXTGEN');
@@ -9,7 +13,7 @@ const logger = Logger.get('NEXTGEN');
 export const handler = async (event: any) => {
   const start = Time.now();
   logger.info(`[RUNNING]`);
-  await loadEnv([NextGenToken, NextGenTokenTrait]);
+  await loadEnv([NextGenCollection, NextGenToken, NextGenTokenTrait]);
   await refreshNextgenMetadata();
   const diff = start.diffFromNow().formatAsDuration();
   logger.info(`[COMPLETE IN ${diff}]`);
