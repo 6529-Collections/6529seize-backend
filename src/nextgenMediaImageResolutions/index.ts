caf6ebc5dc1d132eb11f911f7eac9f56eaeed059
prxt6529 <117092621+prxt6529@users.noreply.github.com>
2024-02-09 17:03:25 +0200

    Update nextgen media upload batch size to 30, include 4k (#311)
    Signed-off-by: prxt <prxt@6529.io>

--- src/nextgenMediaImageResolutions/index.ts
+++ src/nextgenMediaImageResolutions/index.ts
@@ -29,7 +29,7 @@ async function setup() {
 const START_INDEX = 10000000000;
 const END_INDEX = 10000000999;
 
-const BATCH_SIZE = 15;
+const BATCH_SIZE = 30;
 
 export const handler = async () => {
   const start = Time.now();
