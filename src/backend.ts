83b9c7cda340805337b8a946dc2f871655446bf8
prxt <prxt@6529.io>
2024-02-12 09:34:55 +0200

    Remove commented out code
    Signed-off-by: prxt <prxt@6529.io>

--- src/backend.ts
+++ src/backend.ts
@@ -3,17 +3,10 @@ import { Time } from './time';
 
 const logger = Logger.get('BACKEND');
 
-const transactionsLoop = require('./transactionsLoop');
-const nextgenContractLoop = require('./nextgenContractLoop');
-const nftHistoryLoop = require('./nftHistoryLoop');
-
 async function start() {
   const start = Time.now();
   logger.info(`[CONFIG ${process.env.NODE_ENV}] [EXECUTING START SCRIPT...]`);
 
-  // await transactionsLoop.handler();
-  // await nextgenContractLoop.handler();
-  // await nftHistoryLoop.handler();
   const diff = start.diffFromNow().formatAsDuration();
   logger.info(`[START SCRIPT COMPLETE IN ${diff}]`);
   process.exit(0);
