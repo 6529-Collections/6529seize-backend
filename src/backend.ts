1c40021546947ca3895441350c3ecb3e1b27e78d
prxt <prxt@6529.io>
2024-02-19 13:46:45 +0200

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- src/backend.ts
+++ src/backend.ts
@@ -2,12 +2,10 @@ import { Logger } from './logging';
 import { Time } from './time';
 
 const logger = Logger.get('BACKEND');
-const memeLabLoop = require('./memeLabLoop');
 
 async function start() {
   const start = Time.now();
   logger.info(`[CONFIG ${process.env.NODE_ENV}] [EXECUTING START SCRIPT...]`);
-  await memeLabLoop.handler();
 
   const diff = start.diffFromNow().formatAsDuration();
   logger.info(`[START SCRIPT COMPLETE IN ${diff}]`);
