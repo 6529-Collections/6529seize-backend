b1678742f74cc49421d56f96500f673b432614f1
prxt <prxt@6529.io>
2024-02-19 10:34:24 +0200

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- src/backend.ts
+++ src/backend.ts
@@ -2,12 +2,21 @@ import { Logger } from './logging';
 import { Time } from './time';
 
 const logger = Logger.get('BACKEND');
+const transactionsLoop = require('./transactionsLoop');
 const nftsLoop = require('./nftsLoop');
 const ownersLoop = require('./ownersLoop');
+const ownerMetricsLoop = require('./ownerMetricsLoop');
+const tdhLoop = require('./tdhLoop');
 
 async function start() {
   const start = Time.now();
   logger.info(`[CONFIG ${process.env.NODE_ENV}] [EXECUTING START SCRIPT...]`);
+
+  // await transactionsLoop.handler();
+  await nftsLoop.handler();
+  // await ownersLoop.handler();
+  // await ownerMetricsLoop.handler();
+  // await tdhLoop.handler();
 
   const diff = start.diffFromNow().formatAsDuration();
   logger.info(`[START SCRIPT COMPLETE IN ${diff}]`);
