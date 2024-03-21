44cb70e138c7aed8055a002b3c37bd2a483f65db
prxt <prxt@6529.io>
2024-03-21 13:22:33 +0200

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- src/tdhConsolidationsLoop/index.ts
+++ src/tdhConsolidationsLoop/index.ts
@@ -1,7 +1,13 @@
 import { getLastTDH } from '../helpers';
 import { consolidateTDH } from '../tdhLoop/tdh_consolidation';
 import { loadEnv, unload } from '../secrets';
-import { ConsolidatedTDH, TDH } from '../entities/ITDH';
+import {
+  ConsolidatedTDH,
+  ConsolidatedTDHMemes,
+  NftTDH,
+  TDH,
+  TDHMemes
+} from '../entities/ITDH';
 import { Logger } from '../logging';
 import { Time } from '../time';
 import { Profile } from '../entities/IProfile';
@@ -9,6 +15,24 @@ import { fetchAllConsolidationAddresses } from '../db';
 import * as sentryContext from '../sentry.context';
 import { NextGenTokenTDH } from '../entities/INextGen';
 import { CommunityMember } from '../entities/ICommunityMember';
+import {
+  AggregatedActivity,
+  ConsolidatedAggregatedActivity,
+  AggregatedActivityMemes,
+  ConsolidatedAggregatedActivityMemes
+} from '../entities/IAggregatedActivity';
+import { NFTOwner, ConsolidatedNFTOwner } from '../entities/INFTOwner';
+import {
+  OwnerBalances,
+  OwnerBalancesMemes,
+  ConsolidatedOwnerBalances,
+  ConsolidatedOwnerBalancesMemes
+} from '../entities/IOwnerBalances';
+import { consolidateActivity } from '../aggregatedActivityLoop/aggregated_activity';
+import { consolidateNftOwners } from '../nftOwnersLoop/nft_owners';
+import { consolidateOwnerBalances } from '../ownersBalancesLoop/owners_balances';
+import { findTDH } from '../tdhLoop/tdh';
+import { MemesSeason } from '../entities/ISeason';
 
 const logger = Logger.get('TDH_CONSOLIDATIONS_LOOP');
 
@@ -18,8 +42,22 @@ export const handler = sentryContext.wrapLambdaHandler(async () => {
     TDH,
     ConsolidatedTDH,
     NextGenTokenTDH,
+    TDHMemes,
+    ConsolidatedTDHMemes,
     CommunityMember,
-    Profile
+    Profile,
+    NFTOwner,
+    ConsolidatedNFTOwner,
+    OwnerBalances,
+    OwnerBalancesMemes,
+    ConsolidatedOwnerBalances,
+    ConsolidatedOwnerBalancesMemes,
+    AggregatedActivity,
+    ConsolidatedAggregatedActivity,
+    AggregatedActivityMemes,
+    ConsolidatedAggregatedActivityMemes,
+    NftTDH,
+    MemesSeason
   ]);
   const force = process.env.TDH_RESET == 'true';
   logger.info(`[RUNNING force=${force}]`);
@@ -33,8 +71,17 @@ async function consolidatedTdhLoop() {
   const lastTDHCalc = getLastTDH();
   const consolidationAddresses: { wallet: string }[] =
     await fetchAllConsolidationAddresses();
-  await consolidateTDH(
-    lastTDHCalc,
-    consolidationAddresses.map((c) => c.wallet)
-  );
+
+  const distinctWallets = new Set<string>();
+  consolidationAddresses.forEach((address) => {
+    distinctWallets.add(address.wallet);
+  });
+  const walletsArray = Array.from(distinctWallets);
+
+  await findTDH(lastTDHCalc, walletsArray);
+  await consolidateTDH(lastTDHCalc, walletsArray);
+
+  await consolidateNftOwners(distinctWallets);
+  await consolidateOwnerBalances(distinctWallets);
+  await consolidateActivity(distinctWallets);
 }
