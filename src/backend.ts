2dffcf0c6298eaf9f9066bf2385d0966e91e552d
prxt <prxt@6529.io>
2024-04-02 12:19:13 +0300

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- src/backend.ts
+++ src/backend.ts
@@ -2,11 +2,6 @@ import { Logger } from './logging';
 import { Time } from './time';
 import * as dbMigrationsLoop from './dbMigrationsLoop';
 import * as customReplayLoop from './customReplayLoop';
-import * as ownerBalances from './ownersBalancesLoop';
-import * as transactions from './transactionsLoop';
-import * as nftsLoop from './nftsLoop';
-import * as owners from './nftOwnersLoop';
-import * as activity from './aggregatedActivityLoop';
 
 const logger = Logger.get('BACKEND');
 
@@ -14,13 +9,8 @@ async function start() {
   const start = Time.now();
   logger.info(`[CONFIG ${process.env.NODE_ENV}] [EXECUTING START SCRIPT...]`);
 
-  // await nftsLoop.handler(null, null as any, null as any);
-  await transactions.handler(null, null as any, null as any);
-  // await dbMigrationsLoop.handler(null, null as any, null as any);
-  // await customReplayLoop.handler(null, null as any, null as any);
-  await owners.handler(null, null as any, null as any);
-  await ownerBalances.handler(null, null as any, null as any);
-  await activity.handler(null, null as any, null as any);
+  await dbMigrationsLoop.handler(null, null as any, null as any);
+  await customReplayLoop.handler(null, null as any, null as any);
 
   const diff = start.diffFromNow().formatAsDuration();
   logger.info(`[START SCRIPT COMPLETE IN ${diff}]`);
