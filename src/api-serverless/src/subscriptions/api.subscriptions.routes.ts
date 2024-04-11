1f32a952c29e92e88ed486584580fc05d6569535
prxt <prxt@6529.io>
2024-04-11 15:17:52 +0300

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- src/api-serverless/src/subscriptions/api.subscriptions.routes.ts
+++ src/api-serverless/src/subscriptions/api.subscriptions.routes.ts
@@ -333,6 +333,8 @@ router.get(
     >,
     res: Response<any>
   ) {
+    req.setTimeout(300000);
+
     const auth = req.headers.authorization ?? '';
     const contract = req.params.contract;
     const tokenIdStr = req.params.token_id;
