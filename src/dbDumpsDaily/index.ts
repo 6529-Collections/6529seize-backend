90185a0063d1d9c32123b3e1c63ee923ffbc3fde
prxt <prxt@6529.io>
2024-05-07 11:48:59 +0300

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- src/dbDumpsDaily/index.ts
+++ src/dbDumpsDaily/index.ts
@@ -4,6 +4,7 @@ import * as sentryContext from '../sentry.context';
 import {
   CONSOLIDATIONS_TABLE,
   DELEGATIONS_TABLE,
+  NFTDELEGATION_BLOCKS_TABLE,
   TRANSACTIONS_TABLE
 } from '../constants';
 import { sqlExecutor } from '../sql-executor';
@@ -16,9 +17,16 @@ const s3 = new S3Client({ region: 'eu-west-1' });
 export const handler = sentryContext.wrapLambdaHandler(async () => {
   logger.info(`[RUNNING]`);
   await loadEnv([]);
+
   await dumpTable(TRANSACTIONS_TABLE);
   await dumpTable(DELEGATIONS_TABLE);
   await dumpTable(CONSOLIDATIONS_TABLE);
+
+  const nftDelData = await sqlExecutor.execute(
+    `SELECT * FROM ${NFTDELEGATION_BLOCKS_TABLE} ORDER BY block DESC LIMIT 1`
+  );
+  await dumpData(NFTDELEGATION_BLOCKS_TABLE, nftDelData);
+
   await unload();
   logger.info(`[COMPLETE]`);
 });
@@ -27,6 +35,10 @@ async function dumpTable(tableName: string) {
   logger.info(`[TABLE ${tableName}] : [DUMPING...]`);
 
   const data = await sqlExecutor.execute(`SELECT * FROM ${tableName}`);
+  await dumpData(tableName, data);
+}
+
+async function dumpData(tableName: string, data: any) {
   const csv = await converter.json2csvAsync(data);
 
   logger.info(
