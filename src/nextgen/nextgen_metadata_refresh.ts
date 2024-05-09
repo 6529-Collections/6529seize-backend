788f3e2da78b1d774021b0abdfe738b59e3386c0
prxt6529 <117092621+prxt6529@users.noreply.github.com>
2024-05-09 10:16:17 +0300

    Fix nextgen owners (#509)
    Signed-off-by: prxt <prxt@6529.io>

--- src/nextgen/nextgen_metadata_refresh.ts
+++ src/nextgen/nextgen_metadata_refresh.ts
@@ -1,9 +1,11 @@
 import { getDataSource } from '../db';
 import { Logger } from '../logging';
+import { getOwnersForContracts } from '../nftOwnersLoop/owners';
 import {
   fetchNextGenCollections,
   fetchNextGenTokensForCollection
 } from './nextgen.db';
+import { NEXTGEN_CORE_CONTRACT, getNextgenNetwork } from './nextgen_constants';
 import { upsertToken } from './nextgen_core_events';
 import { refreshNextgenTokens } from './nextgen_tokens';
 
@@ -12,6 +14,7 @@ const logger = Logger.get('NEXTGEN_METADATA_REFRESH');
 export async function refreshNextgenMetadata() {
   logger.info(`[RUNNING]`);
   const dataSource = getDataSource();
+  const network = getNextgenNetwork();
   await dataSource.transaction(async (entityManager) => {
     const collections = await fetchNextGenCollections(entityManager);
     for (const collection of collections) {
@@ -20,14 +23,21 @@ export async function refreshNextgenMetadata() {
         entityManager,
         collection
       );
+      const owners = await getOwnersForContracts([
+        NEXTGEN_CORE_CONTRACT[network]
+      ]);
       await Promise.all(
         collectionTokens.map(async (token) => {
+          const owner =
+            owners
+              .find((o) => Number(o.token_id) === Number(token.id))
+              ?.wallet.toLowerCase() ?? token.owner;
           await upsertToken(
             entityManager,
             collection,
             token.id,
             token.normalised_id,
-            token.owner,
+            owner,
             token.mint_date,
             token.mint_price,
             token.burnt_date,
