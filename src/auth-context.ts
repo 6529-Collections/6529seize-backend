f05c642d2874ba264a15683eaa88e9a9950ef202
tarmo <tarmokalling@gmail.com>
2024-05-13 12:33:53 +0300

    Refactored AuthenticationContext interface to a class with helper functions
    Signed-off-by: tarmo <tarmokalling@gmail.com>

--- src/auth-context.ts
+++ src/auth-context.ts
@@ -1,10 +1,49 @@
 import { ApiProfileProxyActionType } from './entities/IProfileProxyAction';
+import { resolveEnum } from './helpers';
 
-export interface AuthenticationContext {
+export class AuthenticationContext {
   readonly authenticatedWallet: string;
   readonly authenticatedProfileId: string | null;
   readonly roleProfileId: string | null;
-  readonly activeProxyActions: AuthenticatedProxyAction[];
+  readonly activeProxyActions: Partial<
+    Record<ApiProfileProxyActionType, AuthenticatedProxyAction>
+  >;
+
+  constructor({
+    authenticatedWallet,
+    authenticatedProfileId,
+    roleProfileId,
+    activeProxyActions
+  }: {
+    readonly authenticatedWallet: string;
+    readonly authenticatedProfileId: string | null;
+    readonly roleProfileId: string | null;
+    readonly activeProxyActions: AuthenticatedProxyAction[];
+  }) {
+    this.authenticatedWallet = authenticatedWallet;
+    this.authenticatedProfileId = authenticatedProfileId;
+    this.roleProfileId = roleProfileId;
+    this.activeProxyActions = activeProxyActions.reduce((acc, action) => {
+      const type = resolveEnum(ApiProfileProxyActionType, action.type);
+      if (type) {
+        acc[type] = action;
+      }
+      return acc;
+    }, {} as Record<ApiProfileProxyActionType, AuthenticatedProxyAction>);
+  }
+
+  public isAuthenticatedAsProxy(): boolean {
+    return (
+      this.roleProfileId !== null &&
+      this.authenticatedProfileId !== this.roleProfileId
+    );
+  }
+
+  public getActingAsId(): string | null {
+    return this.isAuthenticatedAsProxy()
+      ? this.roleProfileId!
+      : this.authenticatedProfileId ?? null;
+  }
 }
 
 export interface AuthenticatedProxyAction {
