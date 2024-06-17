13f3fb6939aa985f789583b184986c57a6fe40fc
prxt <prxt@6529.io>
2024-06-17 13:33:50 +0300

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- src/customReplayLoop/index.ts
+++ src/customReplayLoop/index.ts
@@ -2,12 +2,13 @@ import { loadEnv, unload } from '../secrets';
 import { Logger } from '../logging';
 import * as sentryContext from '../sentry.context';
 import { tdhHistoryLoop } from '../tdhHistoryLoop';
+import { GlobalTDHHistory, TDHHistory } from '../entities/ITDH';
 
 const logger = Logger.get('CUSTOM_REPLAY_LOOP');
 
 export const handler = sentryContext.wrapLambdaHandler(async () => {
   logger.info(`[RUNNING]`);
-  await loadEnv([]);
+  await loadEnv([TDHHistory, GlobalTDHHistory]);
   await replay();
   await unload();
   logger.info('[COMPLETE]');
