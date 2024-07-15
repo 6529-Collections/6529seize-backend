39d5f44bfeacde87851e5a4543e8f6644360fb10
GelatoGenesis <tarmokalling@gmail.com>
2024-07-15 16:30:07 +0300

    Schemas for action events and subscriptions + action event recorder
    Signed-off-by: GelatoGenesis <tarmokalling@gmail.com>

---
+++ src/activity/new-activity-event.ts
@@ -0,0 +1,6 @@
+import { ActivityEventEntity } from '../entities/IActivityEvent';
+
+export interface NewActivityEvent
+  extends Omit<ActivityEventEntity, 'id' | 'created_at' | 'data'> {
+  readonly data: object;
+}
---
+++ src/entities/IIdentitySubscription.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+import { IDENTITY_SUBSCRIPTIONS_TABLE } from '../constants';
+import { ActivityEventAction, ActivityEventTargetType } from './IActivityEvent';
+
+@Entity(IDENTITY_SUBSCRIPTIONS_TABLE)
+@Index(['subscriber_id', 'target_id', 'target_type', 'target_action'], {
+  unique: true
+})
+export class IdentitySubscriptionEntity {
+  @PrimaryGeneratedColumn('increment', { type: 'bigint' })
+  readonly id!: bigint;
+
+  @Index('identity_subscription_subscriber_idx')
+  @Column({ type: 'varchar', length: 50, nullable: false })
+  readonly subscriber_id!: string;
+
+  @Index('identity_subscription_target_id_idx')
+  @Column({ type: 'varchar', length: 50, nullable: false })
+  readonly target_id!: string;
+
+  @Index('identity_subscription_target_type_idx')
+  @Column({ type: 'varchar', length: 50, nullable: false })
+  readonly target_type!: ActivityEventTargetType;
+
+  @Index('identity_subscription_target_action_idx')
+  @Column({ type: 'varchar', length: 50, nullable: false })
+  readonly target_action!: ActivityEventAction;
+}
