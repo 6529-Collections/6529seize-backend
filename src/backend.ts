a9201c872e5872cdcc5f7ccc672399e26228966a
prxt <prxt@6529.io>
2024-07-30 09:23:28 +0300

    WIP
    Signed-off-by: prxt <prxt@6529.io>

--- src/backend.ts
+++ src/backend.ts
@@ -1,31 +1,6 @@
 import { Logger } from './logging';
 import { Time } from './time';
-import { loadEnv } from './secrets';
-import {
-  DropCommentEntity,
-  DropEntity,
-  DropMediaEntity,
-  DropMentionEntity,
-  DropMetadataEntity,
-  DropPartEntity,
-  DropReferencedNftEntity
-} from './entities/IDrop';
-import { DropVoteCreditSpending } from './entities/IDropVoteCreditSpending';
-import { Profile, ProfileArchived } from './entities/IProfile';
-import { CicStatement } from './entities/ICICStatement';
-import { ProfileActivityLog } from './entities/IProfileActivityLog';
-import { Rating } from './entities/IRating';
-import { AbusivenessDetectionResult } from './entities/IAbusivenessDetectionResult';
-import { UserGroupEntity } from './entities/IUserGroup';
-import { RatingsSnapshot } from './entities/IRatingsSnapshots';
-import { ProfileProxyEntity } from './entities/IProfileProxy';
-import { ProfileProxyActionEntity } from './entities/IProfileProxyAction';
-import { WaveEntity } from './entities/IWave';
-import { CookiesConsent } from './entities/ICookieConsent';
-import { AddressConsolidationKey } from './entities/IAddressConsolidationKey';
-import { IdentityEntity } from './entities/IIdentity';
-import { ProfileGroupEntity } from './entities/IProfileGroup';
-import * as dbMigrationsLoop from './dbMigrationsLoop';
+import * as custom from './customReplayLoop';
 
 const logger = Logger.get('BACKEND');
 
@@ -33,37 +8,9 @@ async function start() {
   const start = Time.now();
   logger.info(`[CONFIG ${process.env.NODE_ENV}] [EXECUTING START SCRIPT...]`);
 
-  await loadEnv([
-    Profile,
-    ProfileArchived,
-    CicStatement,
-    ProfileActivityLog,
-    Rating,
-    AbusivenessDetectionResult,
-    RatingsSnapshot,
-    DropEntity,
-    DropPartEntity,
-    DropMentionEntity,
-    DropReferencedNftEntity,
-    DropMetadataEntity,
-    DropMediaEntity,
-    DropVoteCreditSpending,
-    DropCommentEntity,
-    ProfileProxyEntity,
-    ProfileProxyActionEntity,
-    WaveEntity,
-    CookiesConsent,
-    UserGroupEntity,
-    AddressConsolidationKey,
-    IdentityEntity,
-    ProfileGroupEntity
-  ]);
   const diff = start.diffFromNow().formatAsDuration();
-  await dbMigrationsLoop.handler(
-    undefined as any,
-    undefined as any,
-    undefined as any
-  );
+  await custom.handler(undefined as any, undefined as any, undefined as any);
+
   logger.info(`[START SCRIPT COMPLETE IN ${diff}]`);
   process.exit(0);
 }
--- src/customReplayLoop/index.ts
+++ src/customReplayLoop/index.ts
@@ -1,17 +1,35 @@
 import { loadEnv, unload } from '../secrets';
 import { Logger } from '../logging';
 import * as sentryContext from '../sentry.context';
+import { consolidateActivity } from '../aggregatedActivityLoop/aggregated_activity';
+import {
+  AggregatedActivity,
+  ConsolidatedAggregatedActivity,
+  AggregatedActivityMemes,
+  ConsolidatedAggregatedActivityMemes
+} from '../entities/IAggregatedActivity';
+import { MemesSeason } from '../entities/ISeason';
 
 const logger = Logger.get('CUSTOM_REPLAY_LOOP');
 
 export const handler = sentryContext.wrapLambdaHandler(async () => {
   logger.info(`[RUNNING]`);
-  await loadEnv([]);
+  await loadEnv([
+    MemesSeason,
+    AggregatedActivity,
+    ConsolidatedAggregatedActivity,
+    AggregatedActivityMemes,
+    ConsolidatedAggregatedActivityMemes
+  ]);
   await replay();
   await unload();
   logger.info('[COMPLETE]');
 });
 
 async function replay() {
-  logger.info(`[CUSTOM REPLAY NOT IMPLEMENTED]`);
+  // logger.info(`[CUSTOM REPLAY NOT IMPLEMENTED]`);
+  const wallets = new Set<string>();
+  wallets.add('0xf1f476f144df01480297dca47efca565e8b0c9f1');
+
+  await consolidateActivity(wallets);
 }
