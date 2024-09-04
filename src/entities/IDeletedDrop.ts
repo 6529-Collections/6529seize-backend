b4baf0fe0246471481e623846890f1c4e01df70c
GelatoGenesis <tarmokalling@gmail.com>
2024-09-04 12:13:31 +0300

    Leaving a trace of deleted drops and being explicit about them in API
    Signed-off-by: GelatoGenesis <tarmokalling@gmail.com>

---
+++ src/entities/IDeletedDrop.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, Index, PrimaryColumn } from 'typeorm';
+import { DELETED_DROPS_TABLE } from '../constants';
+
+@Entity(DELETED_DROPS_TABLE)
+export class DeletedDropEntity {
+  @PrimaryColumn({ type: 'varchar', length: 100 })
+  readonly id!: string;
+  @Column({ type: 'varchar', length: 100, nullable: false })
+  @Index()
+  readonly wave_id!: string;
+  @Column({ type: 'varchar', length: 100, nullable: false })
+  @Index()
+  readonly author_id!: string;
+  @Column({ type: 'bigint', nullable: false })
+  readonly created_at!: number;
+  @Column({ type: 'bigint', nullable: false })
+  readonly deleted_at!: number;
+}
