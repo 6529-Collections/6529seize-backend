ce4cecf081b7aa8776d01baaf2581168042b7a00
prxt6529 <prxt@6529.io>
2024-09-19 16:06:23 +0300

    Documentation Updates
    Signed-off-by: prxt6529 <prxt@6529.io>

--- src/api-serverless/src/nft-owners/api.nft-owners.db.ts
+++ src/api-serverless/src/nft-owners/api.nft-owners.db.ts
@@ -2,6 +2,35 @@ import { constructFilters } from '../api-helpers';
 import { NFT_OWNERS_CONSOLIDATION_TABLE } from '../../../constants';
 import { fetchPaginated } from '../../../db-api';
 
+export async function fetchAllNftOwners(
+  contract: string | undefined,
+  tokenId: string | undefined,
+  page: number,
+  pageSize: number,
+  sortDir: string
+) {
+  let filters = '';
+  let params: any = {};
+  if (contract) {
+    filters = constructFilters(filters, `contract = :contract`);
+    params.contract = contract;
+  }
+
+  if (tokenId) {
+    filters = constructFilters(filters, `token_id in (:tokenIds)`);
+    params.tokenIds = tokenId.split(',').map((id) => parseInt(id));
+  }
+
+  return await fetchPaginated(
+    NFT_OWNERS_CONSOLIDATION_TABLE,
+    params,
+    `contract ${sortDir}, token_id ${sortDir}`,
+    pageSize,
+    page,
+    filters
+  );
+}
+
 export async function fetchNftOwnersForConsolidation(
   consolidationKey: string,
   contractStr: string | undefined,
