373f92a65ee3c253a5d919279172fb89c32bccd1
GelatoGenesis <tarmokalling@gmail.com>
2024-12-08 13:41:51 +0200

    Connection issue fix on rank/approve vote
    Signed-off-by: GelatoGenesis <tarmokalling@gmail.com>

--- src/drops/vote-for-drop.use-case.ts
+++ src/drops/vote-for-drop.use-case.ts
@@ -56,7 +56,7 @@ export class VoteForDropUseCase {
       );
       return;
     }
-    await this.votingDb.lockAggregateDropRank(drop_id, ctx.connection);
+    await this.votingDb.lockAggregateDropRank(drop_id, ctx);
     const now = Time.now();
     const wave = await this.wavesDb.findById(wave_id, ctx.connection);
     const isRepWave = wave?.voting_credit_type === WaveCreditType.REP;
