17d61162925eb6b06444c65913ff33e4c4c43968
prxt6529 <117092621+prxt6529@users.noreply.github.com>
2024-12-27 10:42:23 +0200

    EULA (#847)
    Signed-off-by: prxt6529 <prxt@6529.io>

---
+++ src/entities/IEULAPolicy.ts
@@ -0,0 +1,14 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm';
+import { EULA_CONSENT_TABLE } from '../constants';
+
+@Entity(EULA_CONSENT_TABLE)
+export class EULAConsent {
+  @PrimaryColumn({ type: 'varchar', length: 100 })
+  device_id!: string;
+
+  @Column({ type: 'bigint' })
+  accepted_at!: number;
+
+  @Column({ type: 'text' })
+  platform!: string;
+}
