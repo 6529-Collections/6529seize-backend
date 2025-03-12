493a5d2999e328c879b953c38dc8c1c4fbc8cc54
GelatoGenesis <tarmokalling@gmail.com>
2025-03-12 17:47:03 +0200

    Websockets support
    Signed-off-by: GelatoGenesis <tarmokalling@gmail.com>

--- src/api-serverless/src/handler.ts
+++ src/api-serverless/src/handler.ts
@@ -1,6 +1,58 @@
 import { app } from './app';
 import * as sentryContext from '../../sentry.context';
+import type {
+  APIGatewayEvent,
+  APIGatewayProxyResult,
+  Context
+} from 'aws-lambda';
+import { appWebSockets, authenticateWebSocketJwt } from './ws/ws';
+import { Logger } from '../../logging';
 
 const serverlessHttp = require('serverless-http');
 
-export const handler = sentryContext.wrapLambdaHandler(serverlessHttp(app));
+const httpHandler = serverlessHttp(app);
+export const handler = sentryContext.wrapLambdaHandler(
+  async (
+    event: APIGatewayEvent,
+    context: Context
+  ): Promise<APIGatewayProxyResult> => {
+    if (event.requestContext && event.requestContext.routeKey) {
+      return wsHandler(event);
+    } else {
+      return httpHandler(event, context);
+    }
+  }
+);
+
+async function wsHandler(
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> {
+  const wsHandlerLogger = Logger.get('wsHandler');
+  const { routeKey, connectionId } = event.requestContext;
+  const { identityId, jwtExpiry } = await authenticateWebSocketJwt(event);
+  switch (routeKey) {
+    case '$connect':
+      try {
+        await appWebSockets.register({
+          identityId,
+          connectionId: connectionId!,
+          jwtExpiry
+        });
+        return { statusCode: 200, body: 'Connected' };
+      } catch (e) {
+        wsHandlerLogger.error(
+          `Failed to connect to websocket (clientId: ${identityId}) Error: ${JSON.stringify(
+            e
+          )}`
+        );
+        return { statusCode: 500, body: 'Failed to connect' };
+      }
+    case '$disconnect':
+      await appWebSockets.deregister({ connectionId: connectionId! });
+      return { statusCode: 200, body: 'Disconnected' };
+    case '$default':
+      return { statusCode: 400, body: 'This websocket does not accept data' };
+    default:
+      return { statusCode: 400, body: 'Unknown routeKey' };
+  }
+}
---
+++ src/api-serverless/src/ws/ws.ts
@@ -0,0 +1,320 @@
+import { Logger } from '../../../logging';
+import * as process from 'node:process';
+import WebSocket from 'ws';
+import type { APIGatewayEvent } from 'aws-lambda';
+import * as passport from 'passport';
+import {
+  wsConnectionRepository,
+  WsConnectionRepository
+} from './ws-connection.repository';
+import { Time } from '../../../time';
+import { profilesService } from '../../../profiles/profiles.service';
+import http from 'http';
+import {
+  ApiGatewayManagementApiClient,
+  DeleteConnectionCommand,
+  PostToConnectionCommand
+} from '@aws-sdk/client-apigatewaymanagementapi';
+
+export class SocketNotAvailableException extends Error {
+  constructor() {
+    super(`Socket is not available`);
+  }
+}
+
+abstract class ClientConnections {
+  private static instance: ClientConnections | null = null;
+
+  static Get(): ClientConnections {
+    if (ClientConnections.instance) {
+      return ClientConnections.instance;
+    }
+    ClientConnections.instance = isDevEnv()
+      ? new WebsocketClientConnections()
+      : new ApiGatewayClientConnections();
+    return ClientConnections.instance;
+  }
+
+  abstract sendMessage({
+    connectionId,
+    message
+  }: {
+    connectionId: string;
+    message: string;
+  }): Promise<void>;
+  abstract closeClient(connectionId: string): Promise<void>;
+}
+
+class WebsocketClientConnections extends ClientConnections {
+  private readonly sockets: Record<string, WebSocket> = {};
+
+  override async sendMessage({
+    connectionId,
+    message
+  }: {
+    connectionId: string;
+    message: string;
+  }) {
+    const socket = this.sockets[connectionId];
+    if (!socket) {
+      throw new SocketNotAvailableException();
+    }
+    try {
+      socket.send(message);
+    } catch (err: any) {
+      throw new SocketNotAvailableException();
+    }
+  }
+
+  override async closeClient(connectionId: string): Promise<void> {
+    const socket = this.sockets[connectionId];
+    if (!socket) {
+      return;
+    }
+    try {
+      socket.close();
+    } catch (err: any) {}
+    delete this.sockets[connectionId];
+  }
+
+  addSocket(connectionId: string, socket: WebSocket) {
+    this.sockets[connectionId] = socket;
+  }
+}
+
+class ApiGatewayClientConnections extends ClientConnections {
+  private readonly logger = Logger.get(ApiGatewayManagementApiClient.name);
+
+  private readonly client: ApiGatewayManagementApiClient;
+
+  constructor() {
+    super();
+    this.client = new ApiGatewayManagementApiClient({
+      endpoint: process.env.API_GATEWAY_WS_ENDPOINT
+    });
+  }
+
+  override async sendMessage({
+    connectionId,
+    message
+  }: {
+    connectionId: string;
+    message: string;
+  }) {
+    try {
+      await this.client.send(
+        new PostToConnectionCommand({
+          ConnectionId: connectionId,
+          Data: Buffer.from(message)
+        })
+      );
+    } catch (err: any) {
+      if (
+        err.name === 'GoneException' ||
+        err.$metadata?.httpStatusCode === 410
+      ) {
+        throw new SocketNotAvailableException();
+      } else {
+        this.logger.error(
+          `Failed to post message to client ${connectionId}: ${JSON.stringify(
+            err
+          )}`
+        );
+      }
+    }
+  }
+
+  override async closeClient(connectionId: string): Promise<void> {
+    try {
+      await this.client.send(
+        new DeleteConnectionCommand({
+          ConnectionId: connectionId
+        })
+      );
+    } catch (err: any) {
+      this.logger.error(`Connection ${connectionId} is already gone.`);
+    }
+  }
+}
+
+export class AppWebSockets {
+  private readonly logger = Logger.get(AppWebSockets.name);
+
+  constructor(
+    private readonly wsConnectionRepository: WsConnectionRepository
+  ) {}
+
+  async send({
+    connectionId,
+    message
+  }: {
+    connectionId: string;
+    message: string;
+  }) {
+    const entity = await this.wsConnectionRepository.getByConnectionId(
+      connectionId,
+      {}
+    );
+    if (!entity || Time.seconds(entity.jwt_expiry).lt(Time.now())) {
+      this.logger.info(
+        `Discovered a stale websocket ${connectionId}. Can't send messages to it. Will close it.`
+      );
+      await this.deregister({ connectionId });
+      return;
+    }
+    try {
+      await ClientConnections.Get().sendMessage({ connectionId, message });
+    } catch (err) {
+      await this.deregister({ connectionId });
+    }
+  }
+
+  async register({
+    identityId,
+    connectionId,
+    jwtExpiry,
+    ws
+  }: {
+    identityId: string;
+    connectionId: string;
+    jwtExpiry: number;
+    ws?: WebSocket;
+  }) {
+    this.logger.info(
+      `Registering socket for client ${identityId} (connectionId: ${connectionId})`
+    );
+    if (isDevEnv()) {
+      if (!ws) {
+        throw new Error('Websocket is required in dec env');
+      }
+      (ClientConnections.Get() as WebsocketClientConnections).addSocket(
+        connectionId,
+        ws
+      );
+    }
+    await this.wsConnectionRepository.save(
+      {
+        identity_id: identityId,
+        jwt_expiry: jwtExpiry,
+        connection_id: connectionId
+      },
+      {}
+    );
+  }
+
+  async deregister({ connectionId }: { connectionId: string }) {
+    this.logger.info(`Deregistering socket for connection ${connectionId}`);
+    await ClientConnections.Get().closeClient(connectionId);
+    await this.wsConnectionRepository.deleteByConnectionId(connectionId, {});
+  }
+}
+
+const ANON_USER_ID = '$ANONONYMOUS_USER$';
+
+export async function authenticateWebSocketJwt(
+  event: APIGatewayEvent
+): Promise<{ identityId: string; jwtExpiry: number }> {
+  const authorizationHeader =
+    event.headers?.Authorization || event.headers?.authorization;
+  let token: string | undefined;
+  if (authorizationHeader?.startsWith('Bearer ')) {
+    token = authorizationHeader.substring('Bearer '.length);
+  } else {
+    token = event.queryStringParameters?.token;
+  }
+
+  if (!token) {
+    return {
+      identityId: ANON_USER_ID,
+      jwtExpiry: Time.now().plusDays(1).toMillis()
+    };
+  }
+
+  const req = {
+    headers: {
+      authorization: `Bearer ${token}`
+    }
+  };
+  const res = {};
+  return new Promise((resolve) => {
+    passport.authenticate(
+      ['jwt', 'anonymous'],
+      { session: false },
+      (err: any, user: any) => {
+        if (err) {
+          return resolve({
+            identityId: ANON_USER_ID,
+            jwtExpiry: Time.now().plusDays(1).toMillis()
+          });
+        }
+        if (!user) {
+          return resolve({
+            identityId: ANON_USER_ID,
+            jwtExpiry: Time.now().plusDays(1).toMillis()
+          });
+        }
+        return profilesService.getProfileByWallet(user.wallet).then((it) =>
+          resolve({
+            identityId: it?.profile?.external_id ?? ANON_USER_ID,
+            jwtExpiry: user.exp ?? Time.now().plusDays(1).toMillis()
+          })
+        );
+      }
+    )(req, res, () => {
+      return resolve({
+        identityId: ANON_USER_ID,
+        jwtExpiry: Time.now().plusDays(1).toMillis()
+      });
+    });
+  });
+}
+
+// mean only for dev environment
+export function mapHttpRequestToGatewayEvent(
+  req: http.IncomingMessage,
+  connectionId: string,
+  routeKey: string
+): APIGatewayEvent {
+  const parsedUrl = new URL(
+    req.url ?? '',
+    `http://${req.headers.host ?? 'localhost'}`
+  );
+  const queryStringParameters: Record<string, string> = {};
+  parsedUrl.searchParams.forEach((value, key) => {
+    queryStringParameters[key] = value;
+  });
+
+  return {
+    body: null,
+    headers: req.headers as Record<string, string>,
+    multiValueHeaders: {},
+    httpMethod: 'GET',
+    isBase64Encoded: false,
+    path: parsedUrl.pathname,
+    queryStringParameters,
+
+    requestContext: {
+      routeKey,
+      connectionId,
+      accountId: 'localAccountId',
+      apiId: 'localApiId',
+      authorizer: undefined,
+      httpMethod: 'GET',
+      identity: undefined,
+      path: parsedUrl.pathname,
+      stage: 'local',
+      requestId: 'localRequestId',
+      requestTimeEpoch: Date.now(),
+      resourceId: 'localResourceId',
+      resourcePath: parsedUrl.pathname
+    },
+    resource: '',
+    stageVariables: undefined
+  } as unknown as APIGatewayEvent;
+}
+
+function isDevEnv(): boolean {
+  return process.env.NODE_ENV === 'local';
+}
+
+export const appWebSockets = new AppWebSockets(wsConnectionRepository);
