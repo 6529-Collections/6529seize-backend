2c1bac7312ce87104ec2a3ebaef698e4aff1721d
prxt6529 <prxt@6529.io>
2025-04-23 10:27:39 +0300

    Subscriptions balance fix custom
    Signed-off-by: prxt6529 <prxt@6529.io>

--- src/backend.ts
+++ src/backend.ts
@@ -2,6 +2,7 @@ import { Logger } from './logging';
 import * as dbMigrationsLoop from './dbMigrationsLoop';
 import { prepEnvironment } from './env';
 import { DataSource } from 'typeorm';
+import * as customReplayLoop from './customReplayLoop';
 
 const logger = Logger.get('BACKEND');
 
@@ -26,13 +27,20 @@ async function syncAllEntities() {
 async function start() {
   logger.info(`[CONFIG ${process.env.NODE_ENV}] [EXECUTING START SCRIPT...]`);
 
-  await syncAllEntities();
+  // await syncAllEntities();
 
-  await dbMigrationsLoop.handler(
+  // await dbMigrationsLoop.handler(
+  //   undefined as any,
+  //   undefined as any,
+  //   undefined as any
+  // );
+
+  await customReplayLoop.handler(
     undefined as any,
     undefined as any,
     undefined as any
   );
+
   process.exit(0);
 }
 
--- src/customReplayLoop/index.ts
+++ src/customReplayLoop/index.ts
@@ -1,6 +1,17 @@
 import { doInDbContext } from '../secrets';
 import { Logger } from '../logging';
 import * as sentryContext from '../sentry.context';
+import { getDataSource } from '../db';
+import {
+  SUBSCRIPTIONS_BALANCES_TABLE,
+  SUBSCRIPTIONS_REDEEMED_TABLE,
+  SUBSCRIPTIONS_TOP_UP_TABLE
+} from '../constants';
+import {
+  RedeemedSubscription,
+  SubscriptionBalance,
+  SubscriptionTopUp
+} from '../entities/ISubscription';
 
 const logger = Logger.get('CUSTOM_REPLAY_LOOP');
 
@@ -9,10 +20,52 @@ export const handler = sentryContext.wrapLambdaHandler(async () => {
     async () => {
       await replay();
     },
-    { logger }
+    {
+      logger,
+      entities: [
+        SUBSCRIPTIONS_REDEEMED_TABLE,
+        SUBSCRIPTIONS_TOP_UP_TABLE,
+        SUBSCRIPTIONS_BALANCES_TABLE
+      ]
+    }
   );
 });
 
 async function replay() {
-  logger.info(`[CUSTOM REPLAY NOT IMPLEMENTED]`);
+  // logger.info(`[CUSTOM REPLAY NOT IMPLEMENTED]`);
+  const redeemedSubscriptions: RedeemedSubscription[] =
+    await getDataSource().query(
+      `SELECT * FROM ${SUBSCRIPTIONS_REDEEMED_TABLE}`
+    );
+
+  const topUps: SubscriptionTopUp[] = await getDataSource().query(
+    `SELECT * FROM ${SUBSCRIPTIONS_TOP_UP_TABLE}`
+  );
+
+  const balances: SubscriptionBalance[] = await getDataSource().query(
+    `SELECT * FROM ${SUBSCRIPTIONS_BALANCES_TABLE}`
+  );
+
+  for (const balance of balances) {
+    const balanceTopUps = topUps.filter((topUp) =>
+      balance?.consolidation_key.includes(topUp.from_wallet)
+    );
+    const totalTopUp = balanceTopUps.reduce(
+      (acc, topUp) => acc + topUp.amount,
+      0
+    );
+
+    const balanceRedeemed = redeemedSubscriptions.filter(
+      (redeemed) => balance?.consolidation_key === redeemed.consolidation_key
+    );
+
+    const rawResult = totalTopUp - balanceRedeemed.length * 0.06529;
+    const balanceAfter = Math.round(rawResult * 1e10) / 1e10;
+
+    logger.info(`[BALANCE] ${balance.consolidation_key} ${balanceAfter}`);
+
+    await getDataSource().query(
+      `UPDATE ${SUBSCRIPTIONS_BALANCES_TABLE} SET balance = ${balanceAfter} WHERE consolidation_key = '${balance.consolidation_key}'`
+    );
+  }
 }
