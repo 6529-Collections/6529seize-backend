39db5e3a83c3b9873f9d1e9480e1246e6324fc92
prxt6529 <117092621+prxt6529@users.noreply.github.com>
2025-04-23 12:49:18 +0300

    Subscriptions check processed (#957)
    Signed-off-by: prxt6529 <prxt@6529.io>

--- src/subscriptionsTopUpLoop/db.subscriptions_topup.ts
+++ src/subscriptionsTopUpLoop/db.subscriptions_topup.ts
@@ -1,4 +1,4 @@
-import { QueryRunner } from 'typeorm';
+import { EntityManager, QueryRunner } from 'typeorm';
 import { updateSubscriptionMode } from '../api-serverless/src/subscriptions/api.subscriptions.db';
 import {
   ADDRESS_CONSOLIDATION_KEY,
@@ -12,8 +12,12 @@ import {
 import { getTransactionLink } from '../helpers';
 import { sendDiscordUpdate } from '../notifier-discord';
 import { sqlExecutor } from '../sql-executor';
+import { Logger } from '../logging';
+
+const logger = Logger.get('SUBSCRIPTIONS_TOP_UP_DB');
 
 export async function persistTopUps(topUps: SubscriptionTopUp[]) {
+  const processedTopUps: SubscriptionTopUp[] = [];
   await sqlExecutor.executeNativeQueriesInTransaction(async (qrHolder) => {
     const queryRunner = qrHolder.connection as QueryRunner;
     const manager = queryRunner.manager;
@@ -21,6 +25,20 @@ export async function persistTopUps(topUps: SubscriptionTopUp[]) {
     const topUpsRepo = manager.getRepository(SubscriptionTopUp);
 
     for (const topUp of topUps) {
+      const isProcessed = await isTopUpProcessed(topUp.hash, manager);
+      if (isProcessed) {
+        const message = `Top up ${topUp.hash} already processed`;
+        logger.warn(message);
+        await sendDiscordUpdate(
+          process.env.SUBSCRIPTIONS_DISCORD_WEBHOOK as string,
+          message,
+          'Subscriptions',
+          'warn'
+        );
+
+        continue;
+      }
+
       let consolidationKey = (
         await manager.query(
           `SELECT consolidation_key FROM ${ADDRESS_CONSOLIDATION_KEY} WHERE address = '${topUp.from_wallet}'`
@@ -54,23 +72,25 @@ export async function persistTopUps(topUps: SubscriptionTopUp[]) {
         try {
           await updateSubscriptionMode(consolidationKey, true, qrHolder);
         } catch (e) {
-          console.warn(
+          logger.warn(
             `Error setting subscription mode to auto-subscribe for ${consolidationKey}: ${e}`
           );
         }
       }
+
+      processedTopUps.push(topUp);
     }
   });
 
-  for (const topUp of topUps) {
+  for (const topUp of processedTopUps) {
     const seizeDomain =
-      process.env.NODE_ENV === 'development' ? 'staging.seize' : 'seize';
+      process.env.NODE_ENV === 'development' ? 'staging.6529' : '6529';
     let discordMessage = `🔝 Subscription Top Up of ${topUp.amount} ETH from ${topUp.from_wallet}.`;
     const link = getTransactionLink(
       parseInt(process.env.SUBSCRIPTIONS_CHAIN_ID ?? '1'),
       topUp.hash
     );
-    discordMessage += ` \n\n[View on Seize] \nhttps://${seizeDomain}.io/${topUp.from_wallet}/subscriptions`;
+    discordMessage += ` \n\n[View on 6529.io] \nhttps://${seizeDomain}.io/${topUp.from_wallet}/subscriptions`;
     discordMessage += ` \n\n[View on Etherscan] \n${link}`;
     await sendDiscordUpdate(
       process.env.SUBSCRIPTIONS_DISCORD_WEBHOOK as string,
@@ -86,4 +106,17 @@ export async function getMaxSubscriptionTopUpBlock(): Promise<number> {
     `SELECT MAX(block) as max_block FROM ${SUBSCRIPTIONS_TOP_UP_TABLE}`
   );
   return result?.[0].max_block ?? 0;
+}
+
+async function isTopUpProcessed(
+  hash: string,
+  manager: EntityManager
+): Promise<boolean> {
+  const exists = await manager
+    .createQueryBuilder()
+    .select('1')
+    .from(SubscriptionTopUp, 'topup')
+    .where('topup.hash = :hash', { hash })
+    .getExists();
+  return exists;
 }
