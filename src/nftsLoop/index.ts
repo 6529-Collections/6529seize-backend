452bd89c3961ce8634526a53df25b9c639909fb4
prxt6529 <prxt@6529.io>
2025-05-08 17:18:19 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- src/nftsLoop/index.ts
+++ src/nftsLoop/index.ts
@@ -19,6 +19,7 @@ import {
   findMemeLabExtendedData
 } from './nft_extended_data';
 import { Transaction } from '../entities/ITransaction';
+import { updateDistributionInfoFor } from './nft_distribution';
 const logger = Logger.get('NFTS_LOOP');
 
 export const handler = sentryContext.wrapLambdaHandler(async (event) => {
@@ -45,59 +46,7 @@ async function nftsLoop(mode?: string) {
   const modeEnum = resolveEnumOrThrow(NFT_MODE, mode);
   await processNFTs(modeEnum);
   await findMemesExtendedData();
-  await updateDistributionInfo();
   await findMemeLabExtendedData();
-}
-
-async function updateDistributionInfo() {
-  const missingInfoDistributions: { contract: string; card_id: number }[] =
-    await getDataSource().manager.query(
-      `SELECT DISTINCT contract, card_id
-        FROM (
-          SELECT contract, card_id, 1 AS tag  FROM ${DISTRIBUTION_NORMALIZED_TABLE} WHERE card_name IS NULL
-        UNION ALL
-        SELECT contract, card_id, 2 AS tag  FROM ${DISTRIBUTION_NORMALIZED_TABLE} WHERE card_name = ''
-        UNION ALL
-        SELECT contract, card_id, 3 AS tag  FROM ${DISTRIBUTION_NORMALIZED_TABLE} WHERE mint_date IS NULL
-      ) AS u`
-    );
-
-  if (missingInfoDistributions.length === 0) {
-    logger.info(`[NO MISSING DISTRIBUTION INFO]`);
-    return;
-  }
-
-  logger.info(
-    `[MISSING INFO DISTRIBUTIONS: ${missingInfoDistributions.length}]`
-  );
-
-  for (const distribution of missingInfoDistributions) {
-    const nft = await getDataSource()
-      .getRepository(NFT)
-      .findOne({
-        where: {
-          contract: distribution.contract,
-          id: distribution.card_id
-        }
-      });
-    if (nft) {
-      await sqlExecutor.execute(
-        `UPDATE ${DISTRIBUTION_NORMALIZED_TABLE}
-          SET card_name = :cardName, mint_date = :mintDate
-          WHERE contract = :contract
-          AND card_id = :cardId;`,
-        {
-          contract: distribution.contract,
-          cardId: distribution.card_id,
-          cardName: nft.name,
-          mintDate: nft.mint_date
-            ? new Date(nft.mint_date)
-                .toISOString()
-                .slice(0, 19)
-                .replace('T', ' ')
-            : null
-        }
-      );
-    }
-  }
+  await updateDistributionInfoFor(NFT);
+  await updateDistributionInfoFor(LabNFT);
 }
