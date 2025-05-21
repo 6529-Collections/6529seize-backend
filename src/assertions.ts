1dc4f8c698852c46c9621874686a739d5d7273e6
GelatoGenesis <tarmokalling@gmail.com>
2025-05-21 17:08:35 +0300

    Fixed a linting issue
    Signed-off-by: GelatoGenesis <tarmokalling@gmail.com>

--- src/assertions.ts
+++ src/assertions.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 export const assertUnreachable = (_x: never): never => {
   // Throw an error with a message indicating that this function should not be reached.
   // This error should only be thrown if there's a bug in the code or a new case has been
