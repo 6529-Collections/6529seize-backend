606450ef2cebe9dbc5c74c0bd2d180cfc947318a
prxt6529 <prxt@6529.io>
2025-12-18 12:58:49 +0200

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- src/tests/subscriptions.test.ts
+++ src/tests/subscriptions.test.ts
@@ -770,6 +770,7 @@ function buildSubscription(
     contract,
     token_id: tokenId,
     subscribed_count: subscribedCount,
+    redeemed: redeemedCount > 0,
     redeemed_count: redeemedCount,
     airdrop_address: airdropAddress,
     balance: 0,
