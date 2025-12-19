b9dd7c452405839da9b737c33ea0e87902ce625f
prxt6529 <prxt@6529.io>
2025-12-19 11:41:57 +0200

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- src/subscriptionsDaily/index.ts
+++ src/subscriptionsDaily/index.ts
@@ -1,7 +1,3 @@
-import { doInDbContext } from '../secrets';
-import { Logger } from '../logging';
-import * as sentryContext from '../sentry.context';
-import { updateSubscriptions } from './subscriptions';
 import {
   NFTFinalSubscription,
   NFTFinalSubscriptionUpload,
@@ -11,14 +7,20 @@ import {
   SubscriptionLog,
   SubscriptionMode
 } from '../entities/ISubscription';
+import { Logger } from '../logging';
+import * as priorityAlertsContext from '../priority-alerts.context';
+import { doInDbContext } from '../secrets';
+import * as sentryContext from '../sentry.context';
+import { updateSubscriptions } from './subscriptions';
 
 const logger = Logger.get('SUBSCRIPTIONS_LOOP');
+const ALERT_TITLE = 'Subscriptions Daily Loop';
 
 export const handler = sentryContext.wrapLambdaHandler(async () => {
   await doInDbContext(
-    async () => {
+    priorityAlertsContext.wrapAsyncFunction(ALERT_TITLE, async () => {
       await updateSubscriptions();
-    },
+    }),
     {
       logger,
       entities: [
