57c9596851317b7b20e0d16f5b0b20b1ec95a373
GelatoGenesis <tarmokalling@gmail.com>
2025-12-22 12:39:23 +0200

    Remove ApiWave.outcomes
    Signed-off-by: GelatoGenesis <tarmokalling@gmail.com>

--- src/api-serverless/src/waves/wave.api.service.ts
+++ src/api-serverless/src/waves/wave.api.service.ts
@@ -117,7 +117,6 @@ export class WaveApiService {
             waveCreationTime,
             createWaveRequest.wave.decisions_strategy
           ),
-          outcomes: [],
           isDirectMessage
         });
         await this.wavesApiDb.insertWave(newEntity, ctxWithConnection);
@@ -1266,7 +1265,6 @@ export class WaveApiService {
             waveUpdateTime,
             request.wave.decisions_strategy
           ),
-          outcomes: [],
           isDirectMessage: waveBeforeUpdate.is_direct_message ?? false
         });
 
