3b6b8fccacce0dda90036d4521eb8ce13ea266fc
prxt6529 <prxt@6529.io>
2026-02-04 17:04:44 +0200

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- src/waveDecisionExecutionLoop/index.ts
+++ src/waveDecisionExecutionLoop/index.ts
@@ -7,7 +7,9 @@ import { dropsDbForWaveDecisions } from '../drops/drops-wave-decisions.db';
 import { Timer } from '../time';
 
 const logger = Logger.get('WAVE_DECISION_EXECUTION_LOOP');
-const waveDecisionsService = createWaveDecisionsService(dropsDbForWaveDecisions);
+const waveDecisionsService = createWaveDecisionsService(
+  dropsDbForWaveDecisions
+);
 
 export const handler = sentryContext.wrapLambdaHandler(async () => {
   await doInDbContext(
