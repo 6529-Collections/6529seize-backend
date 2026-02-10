6884d0177bda5789d8d8b01900510c4146e1057b
prxt6529 <prxt@6529.io>
2026-02-10 11:48:53 +0200

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- src/api-serverless/src/health/health-ui.renderer.ts
+++ src/api-serverless/src/health/health-ui.renderer.ts
@@ -40,11 +40,11 @@ function getStatusDisplayDegraded(): string {
 
 function escapeHtml(value: string): string {
   return value
-    .replace(/&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#39;');
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#39;');
 }
 
 function getRedisStatusClass(redis: HealthData['redis']): string {
