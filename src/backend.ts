a53e1c71974868e860cc401c46b0176689fa7963
prxt6529 <prxt@6529.io>
2026-02-17 16:17:46 +0200

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- src/backend.ts
+++ src/backend.ts
@@ -1,61 +1,16 @@
-// import * as dbMigrationsLoop from './dbMigrationsLoop';
-// import * as claimsBuilder from './claimsBuilder';
-import * as claimsMediaArweaveUploader from './claimsMediaArweaveUploader';
+import * as dbMigrationsLoop from './dbMigrationsLoop';
 import { Logger } from './logging';
-// import * as nftsLoop from './nftsLoop';
-// import { NFT_MODE } from './nftsLoop/nfts';
+
 const logger = Logger.get('BACKEND');
 
 async function start() {
   logger.info(`[CONFIG ${process.env.NODE_ENV}] [EXECUTING START SCRIPT...]`);
 
-  // await dbMigrationsLoop.handler(
-  //   undefined as any,
-  //   undefined as any,
-  //   undefined as any
-  // );
-
-  await claimsMediaArweaveUploader.handler(
-    {
-      Records: [
-        {
-          body: JSON.stringify({
-            meme_id: 458
-          })
-        }
-      ]
-    },
+  await dbMigrationsLoop.handler(
+    undefined as any,
     undefined as any,
     undefined as any
   );
-
-  // await nftsLoop.handler(
-  //   {
-  //     mode: NFT_MODE.DISCOVER
-  //   },
-  //   undefined as any,
-  //   undefined as any
-  // );
-
-  // await claimsBuilder.handler(
-  //   {
-  //     Records: [
-  //       {
-  //         body: JSON.stringify({
-  //           drop_id: '22d78cad-6592-430c-bf16-2337cf511c4b'
-  //         })
-  //       }
-  //     ]
-  //   },
-  //   undefined as any,
-  //   undefined as any
-  // );
-
-  // await transactionsLoop.handler(
-  //   undefined as any,
-  //   undefined as any,
-  //   undefined as any
-  // );
 
   process.exit(0);
 }
