c0a92d68da60b5eb5f0ef29e1841b52703b302ec
prxt6529 <prxt@6529.io>
2026-03-20 10:57:33 +0200

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- src/backend.ts
+++ src/backend.ts
@@ -1,12 +1,17 @@
-import * as dbMigrationsLoop from './dbMigrationsLoop';
+// import * as dbMigrationsLoop from './dbMigrationsLoop';
 import { Logger } from './logging';
-
+import * as customReplayLoop from './customReplayLoop';
 const logger = Logger.get('BACKEND');
 
 async function start() {
   logger.info(`[CONFIG ${process.env.NODE_ENV}] [EXECUTING START SCRIPT...]`);
 
-  await dbMigrationsLoop.handler(
+  // await dbMigrationsLoop.handler(
+  //   undefined as any,
+  //   undefined as any,
+  //   undefined as any
+  // );
+  await customReplayLoop.handler(
     undefined as any,
     undefined as any,
     undefined as any
