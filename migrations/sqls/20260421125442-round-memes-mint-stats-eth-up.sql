ff3ae1dea032237e2dd681fc62369ddf6db3dcc3
prxt6529 <prxt@6529.io>
2026-04-21 16:09:06 +0300

    WIP
    Signed-off-by: prxt6529 <prxt@6529.io>

--- migrations/sqls/20260421125442-round-memes-mint-stats-eth-up.sql
+++ migrations/sqls/20260421125442-round-memes-mint-stats-eth-up.sql
@@ -2,4 +2,5 @@ UPDATE memes_mint_stats
 SET
   proceeds_eth = ROUND(proceeds_eth, 10),
   artist_split_eth = ROUND(artist_split_eth, 10)
-WHERE 1 = 1;
+WHERE memes_mint_stats.proceeds_eth > 0
+   OR memes_mint_stats.artist_split_eth > 0;
