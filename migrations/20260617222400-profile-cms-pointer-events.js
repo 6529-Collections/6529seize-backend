4a5ead8d1d68b3bc239661891eddc4ba01d61fc3
Punk 6529 <108035228+punk6529@users.noreply.github.com>
2026-06-18 14:14:18 +0000

    Fix profile CMS pointer migration execution
    Signed-off-by: Punk 6529 <108035228+punk6529@users.noreply.github.com>

--- migrations/20260617222400-profile-cms-pointer-events.js
+++ migrations/20260617222400-profile-cms-pointer-events.js
@@ -1,16 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
-exports.up = async function (db) {
-  const filePath = path.join(
-    __dirname,
-    'sqls',
-    '20260617222400-profile-cms-pointer-events-up.sql'
-  );
+async function runSqlFile(db, fileName) {
+  const filePath = path.join(__dirname, 'sqls', fileName);
   const data = fs.readFileSync(filePath, { encoding: 'utf8' });
   await db.runSql(data);
+}
+
+exports.up = async function (db) {
+  await runSqlFile(
+    db,
+    '20260617222400-profile-cms-pointer-events-up.sql'
+  );
+  await runSqlFile(
+    db,
+    '20260617222400-profile-cms-publish-signatures-up.sql'
+  );
 };
 
-exports.down = async function () {
-  return null;
+exports.down = async function (db) {
+  await runSqlFile(
+    db,
+    '20260617222400-profile-cms-publish-signatures-down.sql'
+  );
+  await runSqlFile(
+    db,
+    '20260617222400-profile-cms-pointer-events-down.sql'
+  );
 };
