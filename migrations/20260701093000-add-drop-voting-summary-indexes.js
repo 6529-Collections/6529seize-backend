29d0694386e7e0abf3b235790219b2f76285de21
GelatoGenesis <1407802+GelatoGenesis@users.noreply.github.com>
2026-07-01 13:20:32 +0300

    Clean up index migration on failure
    Signed-off-by: GelatoGenesis <1407802+GelatoGenesis@users.noreply.github.com>

--- migrations/20260701093000-add-drop-voting-summary-indexes.js
+++ migrations/20260701093000-add-drop-voting-summary-indexes.js
@@ -17,66 +17,53 @@ function dropIndex(db, sql) {
   return ignoreMysqlError(db.runSql(sql), ['ER_CANT_DROP_FIELD_OR_KEY']);
 }
 
+var INDEXES = [
+  {
+    add: 'ALTER TABLE drop_voter_states ADD INDEX idx_drop_voter_states_drop_votes_voter (drop_id, votes, voter_id), ALGORITHM=INPLACE, LOCK=NONE',
+    drop: 'DROP INDEX idx_drop_voter_states_drop_votes_voter ON drop_voter_states'
+  },
+  {
+    add: 'ALTER TABLE winner_drop_voter_votes ADD INDEX idx_winner_drop_voter_votes_drop_votes_voter (drop_id, votes, voter_id), ALGORITHM=INPLACE, LOCK=NONE',
+    drop: 'DROP INDEX idx_winner_drop_voter_votes_drop_votes_voter ON winner_drop_voter_votes'
+  },
+  {
+    add: 'ALTER TABLE drop_ranks ADD INDEX idx_drop_ranks_wave_vote_last_drop (wave_id, vote, last_increased, drop_id), ALGORITHM=INPLACE, LOCK=NONE',
+    drop: 'DROP INDEX idx_drop_ranks_wave_vote_last_drop ON drop_ranks'
+  },
+  {
+    add: 'ALTER TABLE wave_leaderboard_entries ADD INDEX idx_wave_leaderboard_entries_wave_vote_time_drop (wave_id, vote, timestamp, drop_id), ALGORITHM=INPLACE, LOCK=NONE',
+    drop: 'DROP INDEX idx_wave_leaderboard_entries_wave_vote_time_drop ON wave_leaderboard_entries'
+  },
+  {
+    add: 'ALTER TABLE drops ADD INDEX idx_drops_wave_type_created_id (wave_id, drop_type, created_at, id), ALGORITHM=INPLACE, LOCK=NONE',
+    drop: 'DROP INDEX idx_drops_wave_type_created_id ON drops'
+  }
+];
+
+function cleanupIndexes(db) {
+  return INDEXES.slice()
+    .reverse()
+    .reduce(function(promise, index) {
+      return promise.then(function() {
+        return dropIndex(db, index.drop);
+      });
+    }, Promise.resolve());
+}
+
 exports.up = function(db) {
-  return addIndex(
-    db,
-    'ALTER TABLE drop_voter_states ADD INDEX idx_drop_voter_states_drop_votes_voter (drop_id, votes, voter_id), ALGORITHM=INPLACE, LOCK=NONE'
-  )
-    .then(function() {
-      return addIndex(
-        db,
-        'ALTER TABLE winner_drop_voter_votes ADD INDEX idx_winner_drop_voter_votes_drop_votes_voter (drop_id, votes, voter_id), ALGORITHM=INPLACE, LOCK=NONE'
-      );
-    })
-    .then(function() {
-      return addIndex(
-        db,
-        'ALTER TABLE drop_ranks ADD INDEX idx_drop_ranks_wave_vote_last_drop (wave_id, vote, last_increased, drop_id), ALGORITHM=INPLACE, LOCK=NONE'
-      );
-    })
-    .then(function() {
-      return addIndex(
-        db,
-        'ALTER TABLE wave_leaderboard_entries ADD INDEX idx_wave_leaderboard_entries_wave_vote_time_drop (wave_id, vote, timestamp, drop_id), ALGORITHM=INPLACE, LOCK=NONE'
-      );
-    })
-    .then(function() {
-      return addIndex(
-        db,
-        'ALTER TABLE drops ADD INDEX idx_drops_wave_type_created_id (wave_id, drop_type, created_at, id), ALGORITHM=INPLACE, LOCK=NONE'
-      );
+  return INDEXES.reduce(function(promise, index) {
+    return promise.then(function() {
+      return addIndex(db, index.add);
     });
+  }, Promise.resolve()).catch(function(error) {
+    return cleanupIndexes(db).then(function() {
+      throw error;
+    });
+  });
 };
 
 exports.down = function(db) {
-  return dropIndex(
-    db,
-    'DROP INDEX idx_drops_wave_type_created_id ON drops'
-  )
-    .then(function() {
-      return dropIndex(
-        db,
-        'DROP INDEX idx_wave_leaderboard_entries_wave_vote_time_drop ON wave_leaderboard_entries'
-      );
-    })
-    .then(function() {
-      return dropIndex(
-        db,
-        'DROP INDEX idx_drop_ranks_wave_vote_last_drop ON drop_ranks'
-      );
-    })
-    .then(function() {
-      return dropIndex(
-        db,
-        'DROP INDEX idx_winner_drop_voter_votes_drop_votes_voter ON winner_drop_voter_votes'
-      );
-    })
-    .then(function() {
-      return dropIndex(
-        db,
-        'DROP INDEX idx_drop_voter_states_drop_votes_voter ON drop_voter_states'
-      );
-    });
+  return cleanupIndexes(db);
 };
 
 exports._meta = {
